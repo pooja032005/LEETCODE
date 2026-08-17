eebf81c498b19dcc777ffa5ea8e82609231437bf
POOJA T J <devakipooja9@gmail.com>
2026-08-17 17:21:09 +0530

    Time: 4170 ms (20.17%) | Memory: 12.4 MB (57.46%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findEvenNumbers(self, digits):
+        result = set()
+
+        n = len(digits)
+
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    if i == j or j == k or i == k:
+                        continue
+
+                    if digits[i] == 0:
+                        continue
+
+                    if digits[k] % 2 != 0:
+                        continue
+
+                    number = digits[i] * 100 + digits[j] * 10 + digits[k]
+                    result.add(number)
+
+        return sorted(result)
